739539a904e389a8462c8fe7f0b96e66d7be2713
DanielTran3 <dtran3@ualberta.ca>
2017-02-02 20:16:07 -0700

    Added rwlock

--- server.c
+++ server.c
@@ -12,12 +12,77 @@
 #define STR_LEN 50
 #define READ 0
 #define WRITE 1
-char theArray[NUM_STR][STR_LEN];
 
 typedef struct {
 	int arrayID;
 	int RW;
 } message_t;
+
+typedef struct {
+	int readers;
+	int writer;
+	pthread_cond_t readers_proceed;
+	pthread_cond_t writer_proceed;
+	int pending_writers;
+	pthread_mutex_t read_write_lock;
+} mylib_rwlock_t;
+
+char theArray[NUM_STR][STR_LEN];
+mylib_rwlock_t *synch_threads;
+
+void mylib_rwlock_init (mylib_rwlock_t *l) {
+	l -> readers = l -> writer = l -> pending_writers = 0;
+	pthread_mutex_init(&(l -> read_write_lock), NULL);
+	pthread_cond_init(&(l -> readers_proceed), NULL);
+	pthread_cond_init(&(l -> writer_proceed), NULL);
+}
+
+void mylib_rwlock_rlock(mylib_rwlock_t *l) {
+	/* if there is a write lock or pending writers, perform 
+	condition wait, else increment count of readers and grant 
+	read lock */
+	pthread_mutex_lock(&(l -> read_write_lock));
+	while ((l -> pending_writers > 0) || (l -> writer > 0)) {
+		pthread_cond_wait(&(l -> readers_proceed), &(l -> read_write_lock));
+		l -> readers ++;
+		pthread_mutex_unlock(&(l -> read_write_lock));
+	}
+}
+
+void mylib_rwlock_wlock(mylib_rwlock_t *l) {
+	/* if there are readers or writers, increment pending 
+	writers count and wait. On being woken, decrement pending 
+	writers count and increment writer count */
+	pthread_mutex_lock(&(l -> read_write_lock));
+	while ((l -> writer > 0) || (l -> readers > 0)) {
+		l -> pending_writers ++;
+		pthread_cond_wait(&(l -> writer_proceed), &(l -> read_write_lock));
+		l -> pending_writers --;
+	}
+	l -> writer ++;
+	pthread_mutex_unlock(&(l -> read_write_lock));
+}
+
+void mylib_rwlock_unlock(mylib_rwlock_t *l) {
+	/* if there is a write lock then unlock, else if there 
+	are read locks, decrement count of read locks. If the count 
+	is 0 and there is a pending writer, let it through, else if 
+	there are pending readers, let them all go through */
+	pthread_mutex_lock(&(l -> read_write_lock));
+	if (l -> writer > 0) {
+		l -> writer = 0;
+	}
+	else if (l -> readers > 0) {
+		l -> readers --;
+	}
+	pthread_mutex_unlock(&(l -> read_write_lock));
+	if ((l -> readers == 0) && (l -> pending_writers > 0)) {
+		pthread_cond_signal(&(l -> writer_proceed));
+	}	
+	else if (l -> readers > 0) {
+		pthread_cond_broadcast(&(l -> readers_proceed));
+	}
+}
 
 void *clientThreadHandler(void *args)
 {
@@ -31,10 +96,14 @@ void *clientThreadHandler(void *args)
 	if (draft.RW == WRITE) {
 				
 		snprintf(str, STR_LEN, "String %i has been modified by a write request\n", draft.arrayID);
+		mylib_rwlock_wlock(synch_threads);
 		theArray[draft.arrayID][0] = *str;
+		mylib_rwlock_unlock(synch_threads);	
 	}
 	else {
+		mylib_rwlock_rlock(synch_threads);
 		*str = *theArray[draft.arrayID];
+		mylib_rwlock_unlock(synch_threads);	
 	}
 	printf("sending to client:%s\n", str);
 	write(clientFileDescriptor, str, STR_LEN);
@@ -61,6 +130,10 @@ int main(int argc, char *argv[])
 	sock_var.sin_addr.s_addr = inet_addr("127.0.0.1");
 	sock_var.sin_port = atoi(argv[1]);
 	sock_var.sin_family = AF_INET;
+	
+	synch_threads = malloc(sizeof(mylib_rwlock_t));
+	mylib_rwlock_init(synch_threads);
+
 	if(bind(serverFileDescriptor,(struct sockaddr*)&sock_var,sizeof(sock_var))>=0)
 	{
 		printf("socket has been created\n");
